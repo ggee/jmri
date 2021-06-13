4c06d7bac913e6a158d507798994ce79ec655079
ggee <gee.developer@gmail.com>
2021-06-13 14:18:24 -0400

    Upload correct file

--- FastClockToMQTT/FastClockToMQTT.py
+++ FastClockToMQTT/FastClockToMQTT.py
@@ -15,22 +15,30 @@ from org.python.core.util import StringUtil
 timebase = jmri.InstanceManager.getDefault(jmri.Timebase)
 mqttAdapter = jmri.InstanceManager.getDefault( jmri.jmrix.mqtt.MqttSystemConnectionMemo ).getMqttAdapter()
 topic = "jmri/fastclock/"
+lastHour = 0
 
 class TimeListener(java.beans.PropertyChangeListener):
   def propertyChange(self, event):
+    global lastHour
     print "from", event.oldValue, "to", event.newValue
     mqttAdapter.publish(topic + "time", event.newValue.toString())
-    mqttAdapter.publish(topic + "hour", '{0:02d}'.format(event.newValue.getHours()))
+    if (lastHour != event.newValue.getHours()) :
+      mqttAdapter.publish(topic + "hour", '{0:02d}'.format(event.newValue.getHours()))
+      lastHour = event.newValue.getHours()
     mqttAdapter.publish(topic + "minute", '{0:02d}'.format(event.newValue.getMinutes()))
     return
 
 listener = TimeListener()
 
 def remove_listeners( event ):
+  global timebase
+  print "Stopping MQTT Fast Clock"
   # Remove the listener from the timebase. 
   timebase.removePropertyChangeListener("time", listener)
 
 def add_listeners( event ):
+  global timebase
+  print "Starting MQTT Fast Clock"
   # Attach that listener to the timebase. 
   timebase.addPropertyChangeListener("time", listener)
 
@@ -44,4 +52,3 @@ b1.actionPerformed = remove_listeners
 apps.Apps.buttonSpace().add(b1)
 
 apps.Apps.buttonSpace().revalidate()
-

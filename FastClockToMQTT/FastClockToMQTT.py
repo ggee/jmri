c4ec8640bfafc6110a791044b1968437a62dca99
ggee <gee.developer@gmail.com>
2021-06-13 14:05:41 -0400

    Initial upload

---
+++ FastClockToMQTT/FastClockToMQTT.py
@@ -0,0 +1,47 @@
+# This is an example script to publish fastclock
+# time to MQTT server.
+#
+# Author: Gregory Gee
+
+
+import java
+import java.beans
+import jmri
+import javax.swing.JButton
+import apps
+
+from org.python.core.util import StringUtil
+
+timebase = jmri.InstanceManager.getDefault(jmri.Timebase)
+mqttAdapter = jmri.InstanceManager.getDefault( jmri.jmrix.mqtt.MqttSystemConnectionMemo ).getMqttAdapter()
+topic = "jmri/fastclock/"
+
+class TimeListener(java.beans.PropertyChangeListener):
+  def propertyChange(self, event):
+    print "from", event.oldValue, "to", event.newValue
+    mqttAdapter.publish(topic + "time", event.newValue.toString())
+    mqttAdapter.publish(topic + "hour", '{0:02d}'.format(event.newValue.getHours()))
+    mqttAdapter.publish(topic + "minute", '{0:02d}'.format(event.newValue.getMinutes()))
+    return
+
+listener = TimeListener()
+
+def remove_listeners( event ):
+  # Remove the listener from the timebase. 
+  timebase.removePropertyChangeListener("time", listener)
+
+def add_listeners( event ):
+  # Attach that listener to the timebase. 
+  timebase.addPropertyChangeListener("time", listener)
+
+
+b2 = javax.swing.JButton("Start MQTT Fast Clock")
+b2.actionPerformed = add_listeners
+apps.Apps.buttonSpace().add(b2)
+
+b1 = javax.swing.JButton("Stop MQTT Fast Clock")
+b1.actionPerformed = remove_listeners
+apps.Apps.buttonSpace().add(b1)
+
+apps.Apps.buttonSpace().revalidate()
+

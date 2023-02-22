bc343d3ee93f2312c8356e92942b769480a106c1
ggee <gee.developer@gmail.com>
2023-02-21 22:04:14 -0500

    Remove OTA and use WifiManager
    Remove OTA and replace hard coded wifi with WifiManager setup.

--- FastClockToMQTT/FastClockToMQTT-TM1637.ino
+++ FastClockToMQTT/FastClockToMQTT-TM1637.ino
@@ -1,15 +1,9 @@
 
 #include <ESP8266WiFi.h>
-#include <MQTT.h>
-#include <TM1637Display.h>
-#include <ArduinoOTA.h>
+#include <MQTT.h> // MQTT.h - "MQTT" by Joel Gaehwiler (lwmqtt in Libraries)
+#include <TM1637Display.h> // TM1637Display.h - "TM1637" by Avishay Orpaz
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 
-// Arduino Libraries to load
-// MQTT.h - "MQTT" by Joel Gaehwiler
-// TM1637Display.h - "TM1637" by Avishay Orpaz
-
-const char* ssid = "my_ssid";
-const char* password = "my_pwd";
 const char* mqtt_server = "192.168.10.51";
 const char* node_id = "3997";
 const char* node_name = "node3997";
@@ -26,11 +20,6 @@ int minute = 0;
 TM1637Display display(CLK, DIO);
 
 void connect() {
-  Serial.print("checking wifi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
 
   myIp = WiFi.localIP().toString();
   Serial.println("\nWiFi connected");
@@ -70,49 +59,18 @@ void messageReceived(String &topic, String &payload) {
 
 }
 
-void setup_ota() {
-    // Initialize ATO
-  ArduinoOTA.setHostname(node_name);
-  ArduinoOTA.setPassword("admin");
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { // U_FS
-      type = "filesystem";
-    }
-
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
-  ArduinoOTA.begin();
-
-}
-
 void setup() {
   Serial.begin(115200);
   // Initialize wifi
-  WiFi.begin(ssid, password);
+  WiFiManager wm;
+  if(!wm.autoConnect("AutoConnectAP")) {
+    Serial.println("Failed to connect");
+    ESP.restart();
+  } 
+  else {
+    Serial.println("WiFi connected");
+  }
+  Serial.println("After Wifi setup");
 
   // Set clock LED brightness
   display.setBrightness(0xA);
@@ -121,18 +79,13 @@ void setup() {
   client.begin(mqtt_server, net);
   client.onMessage(messageReceived);
 
-  // Connect to wifi and MQTT server
+  // Connect to MQTT server
   connect();
-
-  // Initialize OTA
-  setup_ota();
 
 }
 
 void loop() {
 
-  // OTA loop
-  ArduinoOTA.handle();
   // MQTT loop
   client.loop();
   // Check connection to MQTT server
